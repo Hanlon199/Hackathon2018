88d78486127ebab81b99aad7251e67cf6be0c544
Ryan Hanlon <rjhanlon@quinnipiac.edu>
2018-11-03 13:01:22 -0400

    homescreen
    homescreen

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -28,6 +28,37 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        var div = document.getElementById("map_canvas");
+
+        // Create a Google Maps native view under the map_canvas div.
+        var map = plugin.google.maps.Map.getMap(div);
+
+        // If you click the button, do something...
+        var button = document.getElementById("button");
+        button.addEventListener("click", function() {
+
+        // Move to the position with animation
+        map.animateCamera({
+          target: {lat: 37.422359, lng: -122.084344},
+          zoom: 17,
+          tilt: 60,
+          bearing: 140,
+          duration: 5000
+        });
+
+        // Add a maker
+        var marker = map.addMarker({
+          position: {lat: 37.422359, lng: -122.084344},
+          title: "Welecome to \n" +
+                 "Cordova GoogleMaps plugin for iOS and Android",
+          snippet: "This plugin is awesome!",
+          animation: plugin.google.maps.Animation.BOUNCE
+        });
+
+        // Show the info window
+        marker.showInfoWindow();
+
+        });
     },
 
     // Update DOM on a Received Event
